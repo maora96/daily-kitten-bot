714ef98359f1f315cf5255d72dd0ab4c1a42ea99
Marina Oliveira <marinaoliveira.r07@gmail.com>
2020-11-16 17:26:23 -0300

    feature: bot working!

---
+++ bot.py
@@ -0,0 +1,58 @@
+import tweepy
+import requests
+import random
+import urllib.request
+import time
+import config
+
+consumer_key = config.api_key
+consumer_secret = config.api_secret
+
+key = config.access_token
+secret = config.token_secret
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(key, secret)
+
+api = tweepy.API(auth)
+
+
+para = [
+    {
+       "breed_id": "sphy" 
+    },
+    {
+        "breed_id": "orie"
+    }
+]
+
+response = requests.get("https://api.thecatapi.com/v1/images/search", params = random.choice(para))
+img_url = response.json()[0]['url']
+filename = img_url.split("/")[-1]
+
+def dl_jpg(url, file_path, file_name):
+    full_path = file_path + file_name
+    urllib.request.urlretrieve(url, full_path)
+
+req = urllib.request.build_opener()
+req.addheaders = [{'User-Agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64)'}]
+urllib.request.install_opener(req)
+
+
+dl_jpg(img_url, "images/", filename)
+
+def tweet_image():
+    interval = 60 * 60 * 3
+    
+    
+
+    while True:
+        print('tweeting kitty pic')
+        api.update_with_media("images/" + filename, "here's a kitten <3")
+        time.sleep(interval)
+
+
+tweet_image()
+
+#api.update_status('heyo from the bot @nyphren', )
+#api.update_with_media("images/" + filename, "here's your daily cat pic!")
